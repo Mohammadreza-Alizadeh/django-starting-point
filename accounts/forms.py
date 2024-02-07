784e3a3bd9ba5835455702b3d50527783702173d
Mohammadreza-Alizadeh <alizadehmohamadreza641@gmail.com>
2024-02-07 03:33:26 +0330

    added views

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-
+from django import forms
 from .models import User
+from django.contrib.auth.forms import AuthenticationForm
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -15,3 +16,34 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = User
         fields = ("email",)
+
+
+
+
+class UserLoginForm(AuthenticationForm):
+    username = forms.EmailField(label='ایمیل', required=True)
+    password = forms.CharField(label='رمز عبور', required=True, widget=forms.PasswordInput())
+
+    """ 
+    you can use crispy forms 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.add_input(Submit('submit', 'ورود',css_class='w-100 text-center'))
+    
+    """
+
+class UserRegisterForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('email', 'password1', 'password2', )
+
+    """
+    use crispy forms for this
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.add_input(Submit('submit', 'ثبت نام',css_class='w-100 text-center'))
+    """
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse_lazy
+from django.contrib import messages
+from .forms import UserLoginForm, UserRegisterForm
+from django.views.generic.edit import FormView
+from django.contrib.auth import login
+
+
+class StudentLoginView(LoginView):
+    redirect_authenticated_user = True
+    template_name = 'accounts/login.html' # you have to create form https://www.geeksforgeeks.org/formview-class-based-views-django/
+    form_class = UserLoginForm 
+
+
+    # def get_success_url(self):
+    #     return reverse_lazy('app:view') 
+    
+    def form_invalid(self, form):
+        return self.render_to_response(self.get_context_data(form=form))
+    
+
+
+class StudentRegisterView(FormView):
+    template_name = 'accounts/register.html' # you have to create form https://www.geeksforgeeks.org/formview-class-based-views-django/
+    form_class = UserRegisterForm
+    redirect_authenticated_user = True
+
+    # success_url = reverse_lazy('app:view')
+    
+    def form_valid(self, form):
+        user = form.save()
+        if user:
+            login(self.request, user)
+        
+        return super(StudentRegisterView, self).form_valid(form)
